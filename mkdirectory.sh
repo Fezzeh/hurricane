986277a100146b8073e406c7caccfa8736e210be
fezzeh <fezzeh.tasliman1987@gmail.com>
2023-08-02 23:12:53 +0330

    a file named mkdirectory.sh added

---
+++ mkdirectory.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# description
+
+read -p "enter your number: " numbers
+
+echo your number is $numbers
+
+for (( number=1; number<$numbers; number++ ))
+do
+mkdir $number
+# rm -rf $number
+done
+
