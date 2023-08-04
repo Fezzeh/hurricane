4f0930337a83a46a72bbc38eb35b7d120f78f8d8
fezzeh <fezzeh.tasliman1987@gmail.com>
2023-08-04 18:39:45 +0330

    01-noop&var.sh is added

---
+++ 01-noop&var.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "what is your favorite color?"
+read
+echo "your favorite color is $REPLY"
+echo "how old are you?"
+read
+echo "you're $REPLY"
+echo "whats your job?"
+read
+echo "you're working as a $REPLY"
+
+
