82fb7597903939f01912202ed77b5da2ca6d3440
fezzeh <fezzeh.tasliman1987@gmail.com>
2023-08-04 18:41:23 +0330

    02-options&variables.sh is added

---
+++ 02-options&variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "what's the product name?"
+
+read ITEM
+
+echo "what  colors are available?"
+
+read color1 color2 color3
+
+echo "the product is $ITEM"
+
+echo "the available colors are $color1, $color2 and $color3"
+
